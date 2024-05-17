f491cb5b3f204f637cbeb240fbc7d0fa99c5d5f8
rd181002 <rd181002@ya.ru>
2024-05-17 09:55:10 +0300

    Minor changes

--- bot_commands.cpp
+++ bot_commands.cpp
@@ -5,21 +5,16 @@
 
 void anymsg(const TgBot::Message::Ptr& message, const TgBot::Bot& bot, const std::unique_ptr<Database>& database)
 {
+    std::jthread([&database, &message](){database->user_update(message->from);});
+
     std::string log_message = std::string(": INFO : BOT : [") + std::to_string(message->from->id) + "] " + message->from->username + " sent command '" + message->text + "'.";
 
     Logger::write(log_message);
-
-    std::jthread([&database, &message](){database->user_update(message->from);});
 }
 
 void noncom(const TgBot::Message::Ptr& message, const TgBot::Bot& bot, const std::unique_ptr<Database>& database)
 {
     if(bot.getApi().blockedByUser(message->chat->id)) return;
-
-    std::jthread([&database, &message](){database->user_update(message->from);});
-
-    if (message->text.starts_with("/start"))
-        return;
 
     bot.getApi().sendMessage(message->chat->id, "They haven't taught me that command.");
 }
