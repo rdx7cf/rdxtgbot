5d89c0b44fb5554e38422fd11995a846c13fac72
rd181002 <rd181002@ya.ru>
2024-05-17 09:14:56 +0300

    removed unnecessary files

--- to_filelog.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <fstream>
-#include <string>
-#include <ctime>
-#include <iomanip>
-#include <thread>
-
-void to_filelog(const std::string&, const std::string& path = "./logs/log.log");
