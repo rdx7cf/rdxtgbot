220da2b6c947450482da626ec8e8cfdcb9d62788
rd181002 <rd181002@ya.ru>
2024-07-28 20:30:47 +0300

    Minor changes

--- ctime++.cpp
+++ ctime++.cpp
@@ -2,11 +2,11 @@
 
 std::tm localtime_ts(std::time_t timer)
 {
-    std::tm t {};
+    std::tm tm {};
 #ifdef __unix__
-    localtime_r(&timer, &t);
+    localtime_r(&timer, &tm);
 #elif _WIN32
-    localtime_s(&timer, &t);
+    localtime_s(&timer, &tm);
 #endif
-    return t;
+    return tm;
 }
