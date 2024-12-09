8b638fa07aa8ce23ae2484f31e223a9b20cb66bd
rd181002 <rd181002@ya.ru>
2024-12-09 23:56:18 +0300

    Minor changes

--- vps.cpp
+++ vps.cpp
@@ -21,34 +21,34 @@ BashCommand VPS::virsh_exec(ACTION a) noexcept
     switch(a)
     {
     case ACTION::INFO:
-        cmd.execute(std::string("virsh dominfo ") + uuid);
+        cmd.execute(std::string("virsh dominfo ") + name);
         break;
     case ACTION::REBOOT:
-        cmd.execute(std::string("virsh destroy ") + uuid + " && virsh start " + uuid);
+        cmd.execute(std::string("virsh destroy ") + name + " && virsh start " + name);
         break;
     case ACTION::SUSPEND:
-        cmd.execute(std::string("virsh suspend ") + uuid);
+        cmd.execute(std::string("virsh suspend ") + name);
         break;
     case ACTION::RESUME:
-        cmd.execute(std::string("virsh resume ") + uuid);
+        cmd.execute(std::string("virsh resume ") + name);
         break;
     case ACTION::RESET:
-        cmd.execute(std::string("virsh reset ") + uuid);
+        cmd.execute(std::string("virsh reset ") + name);
         break;
     case ACTION::SAVE:
-        cmd.execute(std::string("virsh save ") + uuid);
+        cmd.execute(std::string("virsh save ") + name);
         break;
     case ACTION::RESTORE:
-        cmd.execute(std::string("virsh restore ") + uuid);
+        cmd.execute(std::string("virsh restore ") + name);
         break;
     case ACTION::STOP:
-        cmd.execute(std::string("virsh stop ") + uuid);
+        cmd.execute(std::string("virsh stop ") + name);
         break;
     case ACTION::START:
-        cmd.execute(std::string("virsh start ") + uuid);
+        cmd.execute(std::string("virsh start ") + name);
         break;
     case ACTION::NAME:
-        cmd.execute(std::string("virsh domname ") + uuid);
+        cmd.execute(std::string("virsh domname ") + name);
         break;
     }
 
